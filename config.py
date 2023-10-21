c393e5e13829ca854f1260ce7a1ba6e522274ab4
Chintu Sharma <67625427+chintu4@users.noreply.github.com>
2023-10-21 17:10:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 details={
-    'PALM_API':'AIzaSyDwVYvgEaVuAufBeiSCkcbErDVouVpvt50',
-    'emailSender':'lok28800@gmail.com',
-    'emailPassword':"hbqhvgqxnlcbxzai",
+    'PALM_API':'',
+    'emailSender':'',
+    'emailPassword':"",
 }
