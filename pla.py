aba983132cfd1807714333696a0c488b8e991d69
Chintu Sharma <chintusharma00014@gmail.com>
2025-01-15 16:49:17 +0530

    Update pla.py

--- pla.py
+++ pla.py
@@ -1,14 +1,19 @@
-import google.generativeai as palm
+import google.generativeai as genai
 import os
 import config
 
 
 def palm_promt(promptt):
-    palm.configure(api_key=config.details['PALM_API'])
-    response = palm.generate_text(prompt=promptt)
-    #print(response.result) #  'cold.'
-#
-    return response.result
+
+
+    genai.configure(api_key=config.details['PALM_API'])
+
+    # palm.configure(api_key=config.details['PALM_API'])
+    
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    response = model.generate_content(promptt)
+ 
+    return response.text
     
 
 #print(palm_promt("can you craft a professional email to info@digitalscholar.in asking job in their companey [filings] beclear can images required paramenters"))
